a1a6e240ac66af7b95b751d46453089c6059ff75
ahlam <halimaazioui17@gmail.com>
2025-11-02 09:15:37 +0100

    premier commit

---
+++ src/main/java/com/buvette/buvette_backend/BuvetteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.buvette.buvette_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuvetteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuvetteBackendApplication.class, args);
+	}
+
+}
