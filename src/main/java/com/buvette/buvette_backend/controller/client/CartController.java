c2610c406743d6e84c7829577e1b26c0774d26e7
Ikrame Aazi <ikrameaazi@gmail.com>
2026-01-31 21:14:37 +0100

    unavailable items cant be added to cart.

--- src/main/java/com/buvette/buvette_backend/controller/client/CartController.java
+++ src/main/java/com/buvette/buvette_backend/controller/client/CartController.java
@@ -2,7 +2,9 @@ package com.buvette.buvette_backend.controller.client;
 
 import com.buvette.buvette_backend.model.client.CartItem;
 import com.buvette.buvette_backend.model.client.CartUpdateRequest;
+import com.buvette.buvette_backend.model.client.MenuItem;
 import com.buvette.buvette_backend.model.client.User;
+import com.buvette.buvette_backend.repository.client.MenuItemRepository;
 import com.buvette.buvette_backend.repository.shared.UserRepository;
 
 import org.springframework.http.ResponseEntity;
@@ -18,9 +20,11 @@ import java.util.List;
 public class CartController {
 
     private final UserRepository userRepository;
+    private final MenuItemRepository itemRepository;
 
-    public CartController(UserRepository userRepository) {
+    public CartController(UserRepository userRepository,MenuItemRepository itemRepository) {
         this.userRepository = userRepository;
+        this.itemRepository = itemRepository;
     }
 
     // GET CART
@@ -42,12 +46,20 @@ public class CartController {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
-        List<CartItem> cart = user.getCart();
+        MenuItem item = itemRepository.findById(request.getItemId())
+                .orElseThrow(() -> new RuntimeException("Item not found"));
 
+        if (!item.getAvailability()) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("This item is currently unavailable and cannot be added to cart");
+        }
+
+        List<CartItem> cart = user.getCart();
         CartItem existing = cart.stream()
-            .filter(c -> c.getItemId().equals(request.getItemId()))
-            .findFirst()
-            .orElse(null);
+                .filter(c -> c.getItemId().equals(request.getItemId()))
+                .findFirst()
+                .orElse(null);
 
         if (request.getQuantity() <= 0) {
             if (existing != null) cart.remove(existing);
@@ -60,7 +72,6 @@ public class CartController {
         }
 
         userRepository.save(user);
-
         return ResponseEntity.ok(cart);
     }
 
